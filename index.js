e7d093e7b1cf6f7a5901692e529cacd6d94f7971
github username <github email address>
2018-08-27 20:52:02 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+
+const companyName = 'Scuber';
+try {
+  string = 'Strout';
+} catch(err) {
+  console.log(err);
+}
+
+const number = 42;
+
+
+let mostProfitableNeighborhood = 'Chelsea'
+
+let companyCeo = 'Susan Smith'
